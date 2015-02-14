f2926056b6ca694af01f8e19110397895544cb2d
Justin Catterson <justin.catterson@gmail.com>
2015-02-14 12:32:17 -0500

    No longer using the ng-view.  Using ng-include instead.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,6 @@
 var app = angular.module("app", ["ngResource", "ngRoute"]);
 app.config(['$routeProvider', function($routeProvider){
-  $routeProvider.otherwise({
+  $routeProvider.otherwise( {
     templateUrl: '../templates/sobject_list.html',
     controller: 'SalesforceCtrl'
   });
