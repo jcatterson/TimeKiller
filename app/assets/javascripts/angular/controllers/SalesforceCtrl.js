be8aed9622468fce8d7dae53716f1835e967ab65
Justin Catterson <justin.catterson@gmail.com>
2015-02-15 08:03:54 -0500

    Displaying a code-like text area for the user to write queries

--- app/assets/javascripts/angular/controllers/SalesforceCtrl.js
+++ app/assets/javascripts/angular/controllers/SalesforceCtrl.js
@@ -11,6 +11,10 @@ app.controller("SalesforceCtrl", ['$scope', "$resource", function($scope, $resou
   var Describe = $resource( '/salesforce/describe', {}, {query: describe} );
   var original_sobject_list;
   var described_objects = {}
+  var codeWindow = CodeMirror.fromTextArea( document.getElementById( 'soql_query' ), {
+    lineNumbers: true
+  });
+
   $scope.sobjects = Salesforce.query( function(result){
     original_sobject_list = _.clone( result );
   });
@@ -24,15 +28,25 @@ app.controller("SalesforceCtrl", ['$scope', "$resource", function($scope, $resou
       description = Describe.query({"sobject":sobject_to_describe}, function(res){
         described_objects[sobject_to_describe] = res;
         $scope.described_sobject = res;
+        initializeSOQL();
       });
     }
     else{
       $scope.described_sobject = described_objects[sobject_to_describe];
+      initializeSOQL();
     }
   }
 
+  initializeSOQL = function(){
+    var describedObject = $scope.described_sobject;
+    var first_field = describedObject.sobject.fields[0];
+    codeWindow.setValue( "Select " + first_field.name + ", from " + describedObject.sobject.name );
+    codeWindow.focus();
+    window.txt = codeWindow;
+    codeWindow.doc.setCursor( {line:0, ch:"Select ".length + first_field.name.length + 1} );
+  }
+
   sobject_is_like_search_term = function( sobject ){
-    window.sobject = $scope.sobject_search;
     return sobject.toUpperCase().indexOf( $scope.sobject_search.toUpperCase() ) >= 0;
   }
 }])
