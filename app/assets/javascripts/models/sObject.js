a36619a6b15a2ad76000df78dd22e316c856840c
Justin Catterson <justin.catterson@gmail.com>
2015-02-23 15:31:28 -0500

    Tests are passing, but the logic does not appear to be

--- app/assets/javascripts/models/sObject.js
+++ app/assets/javascripts/models/sObject.js
@@ -20,6 +20,7 @@ sObject = function( sobjectHash, metadatas ){
   this.getFieldValue = function( column ){
     var tableName = [];
     if( column.isSubQuery ){
+      console.log( "The column is a sub query " );
       tableName.push( column.parentName );
     }
     var fieldLookups = tableName.concat( column.name.toUpperCase().split('.') );
@@ -28,24 +29,37 @@ sObject = function( sobjectHash, metadatas ){
       var field = fieldLookups[i];
 
       var cols = currentObject.listColumnNames();
+      console.log( "Field" + field );
+      console.log( "What are my cols?" + cols );
       var index = _.findIndex( cols, function(col){
                     return col.toUpperCase() == field;
                   });
       var colName = cols[index];
+      console.log( "What is the cols name?" + colName );
+      console.log( colName );
       currentObject = currentObject.hash[ colName ];
+      console.log( "What is my curentObject" );
+      console.log( currentObject );
       if( !currentObject ){
         currentObject = '';
         break;
       }
       else if( currentObject.constructor === Array ){
         var values = [];
+        console.log("Current Object: " );
+        console.log( currentObject );
         for( var x = 0; x < currentObject.length; x++ ){
+          alert( "We changed it!");
           column.isSubQuery = false;
-          values.push( new sObject( currentObject[x], this.metadatas ).getFieldValue( column ) );
+          var childRec = new sObject( currentObject[x], this.metadatas );
+          var answers = childRec.getFieldValue( column );
+          values.push( answers );
         }
+        console.log("What are my values?" + values );
         return values;
       }
       else if( typeof currentObject === 'object' ){
+        console.log( "We are an sObject ");
         currentObject = new sObject( currentObject );
       }
     }
