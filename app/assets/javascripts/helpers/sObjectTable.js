4656536bd4bc02f7c9197ead46385fb5ecb8c5fb
Justin Catterson <justin.catterson@gmail.com>
2015-02-24 21:50:48 -0500

    sObject Table should be able to now display headers

--- app/assets/javascripts/helpers/sObjectTable.js
+++ app/assets/javascripts/helpers/sObjectTable.js
@@ -24,4 +24,53 @@ function sObjectTable( queryString, sObjects ){
         }
         return headerRow;
     }
+
+    this.getHeaders = function(){
+        var headers = this.createHeaders();
+        var actualHeaders = [];
+        actualHeaders.push( [] );
+        var standardHeaders = [];
+        var innerHeaders = [];
+        for( var i = 0; i < headers.length; i++ ){
+            var header = headers[i];
+            var th = {
+                rowSpan: 1,
+            }
+            if( _.isObject( header ) ){
+                th.name = header.tableName;
+                th.colSpan = header.headers.length;
+                th.subHeaders = header.headers;
+                innerHeaders.push( th );
+            }
+            else{
+                th.name = header;
+                th.colSpan = 1;
+                standardHeaders.push( th );
+            }
+            actualHeaders[0].push( th );
+        }
+        if( actualHeaders[0].length != standardHeaders.length ){// We have duplicates
+            for( var i = 0; i < standardHeaders.length; i++ ){
+                var header = standardHeaders[i];
+                header.rowSpan = 2;
+            }
+        }
+        if( innerHeaders.length ){
+            actualHeaders.push( [] );
+        }
+        for( var i = 0; i < innerHeaders.length; i++ ){
+            var innerHeader = innerHeaders[i];
+            for( var j = 0; j < innerHeader.subHeaders.length; j++ ){
+                var innerHeaderColumn = innerHeader.subHeaders[j];
+                var header = {
+                    name: innerHeaderColumn,
+                    colSpan: 1,
+                    rowSpan: 1
+                };
+                actualHeaders[1].push( header );
+            }
+        }
+
+        return actualHeaders;
+    }
 }
--- spec/javascripts/helpers/sObjectTableSpec.js
+++ spec/javascripts/helpers/sObjectTableSpec.js
@@ -1,7 +1,7 @@
 describe("sObjectTable", function() {
-    var table, query;
+    var table, query, innerQuery;
     beforeEach(function(){
-        var innerQuery = 'Select Id,StageName from Opportunities';
+        innerQuery = 'Select Id,StageName from Opportunities';
         innerQuery = simpleSqlParser.sql2ast( innerQuery.toUpperCase() );
         query = "Select Id FROM Account".toUpperCase();
         query = simpleSqlParser.sql2ast( query );
@@ -20,6 +20,21 @@ describe("sObjectTable", function() {
             var headers = table.createHeaders();
             expect( 2 ).toEqual( headers[1].headers.length );
             expect( "OPPORTUNITIES" ).toEqual( headers[1].tableName );
+        });
+    });
+
+    describe(".getHeaders", function(){
+        it("when inner queries exist, standard rows to have a rowspan of 2 ", function(){
+            var headers = table.getHeaders();
+            expect(2).toEqual( headers[0][0].rowSpan );
+        });
+        it("when inner queries exist, inner query rows to have a rowspan of 1", function(){
+            var headers = table.getHeaders();
+            expect(1).toEqual( headers[0][1].rowSpan );
+        });
+        it("when inner queries to have a colSpan the same length as the fields in the query", function(){
+            var headers = table.getHeaders();
+            expect( innerQuery["SELECT"].length ).toEqual( headers[0][1].colSpan );
         });
     });
 });
