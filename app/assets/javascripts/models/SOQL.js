8950752c5f58f5935b370dd1382c6b71ae1b2aad
Justin Catterson <justin.catterson@gmail.com>
2015-02-28 09:53:21 -0500

    Support for aggregate clauses

--- app/assets/javascripts/models/SOQL.js
+++ app/assets/javascripts/models/SOQL.js
@@ -6,7 +6,7 @@ SOQL = function( queryString ){
         for( var i = 0; i < columns.length; i++ ){
           var col = columns[i].name;
           if( col.indexOf("(") == 0 && col.lastIndexOf(")") == col.length - 1){
-            var innerQuery = col.substring(1, col.length-1);
+            var innerQuery = col.substring(1, col.length - 1);
             columns[i] = new SOQL( innerQuery );
           }
         }
@@ -19,10 +19,23 @@ SOQL = function( queryString ){
 
     this.dataColumns = function(){
       var cols = [];
+      var numAiliases = 0;
       for( var i = 0; i < this.columns().length; i++ ){
         var col = this.columns()[i];
-        if( col.name ){
-          cols.push( col.name );
+        var colName = col.name;
+        if( colName ){
+          var aggregateMatch = /([(][ ]*[A-Za-z_0-9]+[ ]*[)])[ ]*([A-Za-z0-9]*)/;
+          if( aggregateMatch.test( colName ) ){
+            var matches = aggregateMatch.exec( colName );
+            if( matches.length == 3 && matches[2] != "" ){// We have an ailias
+              colName = matches[2];
+            }
+            else{// we have an aggregate without an ailias
+              colName = 'EXPR' + numAiliases;
+              numAiliases++;
+            }
+          }
+          cols.push( colName );
         }
         else{
           var subCols = col.dataColumns();
--- spec/javascripts/models/SOQLSpec.js
+++ spec/javascripts/models/SOQLSpec.js
@@ -5,12 +5,62 @@ describe("soql", function() {
         beforeEach( function(){
             soql = new SOQL( queryString );
         });
-        
+
         describe(".columns", function(){
             it( "shows all columns from query", function(){
                 expect( soql.columns()[0].name ).toEqual( "ID" );
                 expect( soql.columns().length ).toEqual( 1 );
             });
+        });
+
+        describe(".tableName", function(){
+            it("expect the from clause name", function(){
+                expect( soql.tableName() ).toEqual("ACCOUNT")
+            });
+        });
+    });
+
+    describe("with inner query", function(){
+        var queryString = "Select Id, (Select Id from Opportunities) from Account";
+        var soql;
+        beforeEach( function(){
+            soql = new SOQL( queryString );
+        });
+
+        describe( ".columns", function(){
+            it("display inner query as a single column", function(){
+                expect( soql.columns()[1].tableName() ).toEqual( "OPPORTUNITIES" );
+            });
+        });
+
+        describe( ".dataColumns", function(){
+            it("display all columns that can retrive data on a sObject", function(){
+                expect( soql.dataColumns() ).toEqual(["ID", "OPPORTUNITIES.ID"]);
+            });
+        });
+    });
+
+    describe( "aggregate query as aliased name", function(){
+        var queryString = "Select count(ID) test from Account";
+        var soql;
+        beforeEach( function(){
+            soql = new SOQL( queryString );
+        });
+
+        it( "the ailiased name is a data column", function(){
+            expect( soql.dataColumns() ).toEqual( ["TEST"] );
+        });
+    });
+
+    describe( "aggregate query without ailias", function(){
+        var queryString = "Select Min(CreatedDate), MAX(AccountNumber) from Account";
+        var soql;
+        beforeEach( function(){
+            soql = new SOQL( queryString );
+        });
+
+        it( "non-ailiased aggregate columns shall be named \"EXPRn\"", function(){
+            expect( soql.dataColumns() ).toEqual( ["EXPR0", "EXPR1"] );
         });
     });
 });
