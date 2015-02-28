d0b821b58f122e2aa1e2924ee70e50c8c8cb947b
Justin Catterson <justin.catterson@gmail.com>
2015-02-28 10:37:44 -0500

    Moving directives that can be seperated from the SalesforceCtrl

--- app/assets/javascripts/controllers/SalesforceCtrl.js
+++ app/assets/javascripts/controllers/SalesforceCtrl.js
@@ -58,43 +58,7 @@ app.controller("SalesforceCtrl", ['$scope', "$resource", function($scope, $resou
 }])
 .directive('sobjectList', function(){
   return{
+    restrict: 'E',
     templateUrl: '/templates/salesforce/sobject_list.html'
   }
-})
-.directive('sobjectResults', function(){
-  return{
-    restrict: 'E',
-    scope: {
-      queryInfo: '=query',
-      sObjects: '=sobjects'
-    },
-    templateUrl: '/templates/salesforce/sobject_results.html'
-  };
-})
-.directive('uiCodeMirror', [
-  function(){
-    return {
-      restrict: "A",
-      link: function( scope, element, attrs ){
-        CodeMirror.commands.autocomplete = function(cm){
-          cm.showHint({
-                        hint: CodeMirror.hint.soql,
-                        options: {
-                          tables: {
-                            "abc":["Test", "TEst"],
-                            "def":["1", "2", "3"]
-                          },
-                          defaultTable: "abc"
-                        }
-                    });
-        }
-
-        scope.codeWindow = CodeMirror.fromTextArea( element[0], {
-          lineNumbers: true,
-          extraKeys: {"Ctrl-Space": "autocomplete"},
-          mode: "sql"
-        });
-      }
-    }
-  }
-]);
+});
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,3 +4,40 @@ app.config(['$routeProvider', function($routeProvider){
     templateUrl: '../templates/index.html'
   });
 }]);
+app.directive('sobjectResults', function(){
+  return{
+    restrict: 'E',
+    scope: {
+      queryInfo: '=query',
+      sObjects: '=sobjects'
+    },
+    templateUrl: '/templates/salesforce/sobject_results.html'
+  };
+})
+.directive('uiCodeMirror', [
+  function(){
+    return {
+      restrict: "A",
+      link: function( scope, element, attrs ){
+        CodeMirror.commands.autocomplete = function(cm){
+          cm.showHint({
+                        hint: CodeMirror.hint.soql,
+                        options: {
+                          tables: {
+                            "abc":["Test", "TEst"],
+                            "def":["1", "2", "3"]
+                          },
+                          defaultTable: "abc"
+                        }
+                    });
+        }
+
+        scope.codeWindow = CodeMirror.fromTextArea( element[0], {
+          lineNumbers: true,
+          extraKeys: {"Ctrl-Space": "autocomplete"},
+          mode: "sql"
+        });
+      }
+    }
+  }
+]);
